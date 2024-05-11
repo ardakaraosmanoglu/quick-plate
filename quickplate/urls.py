c9afbe0ab6c75dda8a204f18af20b9b13b856988
ardakaraosmanoglu <ardakaraosmanoglu7@gmail.com>
2024-05-11 22:22:30 +0300

    chore: Add URLs for menu, orders, users, and core apps

--- quickplate/urls.py
+++ quickplate/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("menu/", include("menu.urls")),
+    path("orders/", include("orders.urls")),
+    path("users/", include("users.urls")),
+    path("core/", include("core.urls")),
 ]
 
 
