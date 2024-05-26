601cf51e303434e3919f4f808627b9c738bec7e9
ardakaraosmanoglu <ardakaraosmanoglu7@gmail.com>
2024-05-27 00:08:25 +0300

    Refactor menu header to remove unnecessary image and search button

--- quickplate/urls.py
+++ quickplate/urls.py
@@ -19,11 +19,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib import admin
+from django.urls import path, include
+from . import views  # Import your views
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("menu/", include("menu.urls")),
+    path('admin/', admin.site.urls),
+    path('menu/', include('menu.urls')),  # Assuming 'menu' is your app name
+    path('', views.index, name='index'),  # Add this line for the root URL
+    
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
